9658a37b7d0b44b8d123c080589c8be48a6bfdb7
unknown <christysimarmata7@gmail.com>
2022-08-08 20:20:22 +0700

    Adding custom navigation drawer

--- app/src/main/java/com/example/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/example/cityguide/User/UserDashboard.java
@@ -1,12 +1,19 @@
 package com.example.cityguide.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.cityguide.HelperClasses.HomeAdapter.CategoriesAdapter;
 import com.example.cityguide.HelperClasses.HomeAdapter.CategoriesHelperClass;
@@ -15,28 +22,99 @@ import com.example.cityguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.cityguide.HelperClasses.HomeAdapter.MostViewedAdapter;
 import com.example.cityguide.HelperClasses.HomeAdapter.MostViewedHelperClass;
 import com.example.cityguide.R;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    static final float END_SCALE = 0.7f;
 
     RecyclerView featuredRecycler, mostViewedRecycler, categoriesRecycler;
     RecyclerView.Adapter adapter;
     private GradientDrawable gradient1, gradient2, gradient3, gradient4;
+    ImageView menuIcon;
+    LinearLayout contentView;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
 
         featuredRecycler = findViewById(R.id.featured_recycler);
         mostViewedRecycler = findViewById(R.id.most_viewed_recycler);
         categoriesRecycler = findViewById(R.id.categories_recycler);
+        menuIcon = findViewById(R.id.menu_icon);
+        contentView = findViewById(R.id.content);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+        navigationDrawer();
 
         featuredRecycler();
         mostViewedRecycler();
         categoriesRecycler();
+    }
+
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+        drawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimary));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 
     private void featuredRecycler() {
@@ -80,16 +158,17 @@ public class UserDashboard extends AppCompatActivity {
         gradient3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xfff7c59f, 0xFFf7c59f});
         gradient4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffb8d7f5, 0xffb8d7f5});
 
-        categories.add(new CategoriesHelperClass(gradient1,R.drawable.mcdonalds, "McDonald's"));
-        categories.add(new CategoriesHelperClass(gradient2,R.drawable.cafe1, "Cofee Shop"));
-        categories.add(new CategoriesHelperClass(gradient3,R.drawable.cafe2, "Restaurants"));
-        categories.add(new CategoriesHelperClass(gradient4,R.drawable.transport, "Transport"));
-        categories.add(new CategoriesHelperClass(gradient1,R.drawable.education, "Education"));
+        categories.add(new CategoriesHelperClass(gradient1, R.drawable.mcdonalds, "McDonald's"));
+        categories.add(new CategoriesHelperClass(gradient2, R.drawable.cafe1, "Cofee Shop"));
+        categories.add(new CategoriesHelperClass(gradient3, R.drawable.cafe2, "Restaurants"));
+        categories.add(new CategoriesHelperClass(gradient4, R.drawable.transport, "Transport"));
+        categories.add(new CategoriesHelperClass(gradient1, R.drawable.education, "Education"));
 
         adapter = new CategoriesAdapter(categories);
         categoriesRecycler.setAdapter(adapter);
 
 
-
     }
+
+
 }
